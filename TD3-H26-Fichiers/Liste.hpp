9f18dfca60fad91e5f533de36462f372ff581ceb
macos <macos@macoss-MacBook-Pro.local>
2026-02-20 08:56:01 -0500

    TD3

---
+++ TD3-H26-Fichiers/Liste.hpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+//TODO: Rentdre la liste générique.
+class Liste
+{
+public:
+	//TODO: Constructeurs et surcharges d'opérateurs
+
+	//TODO: Méthode pour ajouter un élément à la liste
+
+	// Pour size, on utilise le même nom que les accesseurs de la bibliothèque standard, qui permet d'utiliser certaines fonctions de la bibliotheque sur cette classe.
+	unsigned size() const         { return nElements_; }
+	unsigned getCapacite() const  { return capacite_; }
+
+	//TODO: Méthode pour changer la capacité de la liste
+
+	//TODO: Méthode pour trouver un élément selon un critère (lambda).
+
+private:
+	unsigned nElements_;
+	unsigned capacite_;
+	//TODO: Attribut contenant les éléments de la liste.
+};
