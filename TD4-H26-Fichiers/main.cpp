c0e56f0d998789503f07e8effd6c50b79043d938
rolandtchokpon <104642468+rolandtchokpon@users.noreply.github.com>
2026-02-26 20:08:12 -0500

    Nouveau repertoire de travail

---
+++ TD4-H26-Fichiers/main.cpp
@@ -0,0 +1,68 @@
+﻿#include <fstream>
+#include "bibliotheque_cours.hpp"
+using namespace std;
+
+using UInt8  = uint8_t;
+using UInt16 = uint16_t;
+
+UInt8 lireUint8(istream& fichier)
+{
+	UInt8 valeur = 0;
+	fichier.read(reinterpret_cast<char*>(&valeur), sizeof(valeur));
+	return valeur;
+}
+
+UInt16 lireUint16(istream& fichier)
+{
+	UInt16 valeur = 0;
+	fichier.read(reinterpret_cast<char*>(&valeur), sizeof(valeur));
+	return valeur;
+}
+
+string lireString(istream& fichier)
+{
+	string texte;
+	texte.resize(lireUint16(fichier));
+	fichier.read(reinterpret_cast<char*>(&texte[0]), streamsize(sizeof(texte[0])) * texte.length());
+	return texte;
+}
+
+template <class T>
+concept Dereferencable = requires (T&& t) { *std::forward<T>(t); };
+
+// Surcharge quand c'est un pointeur
+template <Dereferencable T> 
+decltype(auto) as_ref(T&& t)
+{
+	return *std::forward<T>(t);
+}
+
+// Surcharge moins contrainte (donc quand ce n'est pas un pointeur)
+template <class T>
+T&& as_ref(T&& t) {
+	return std::forward<T>(t);
+}
+
+
+int main()
+{
+	#pragma region "Bibliothèque du cours"
+	// Permet sous Windows les "ANSI escape code" pour changer de couleur
+	// https://en.wikipedia.org/wiki/ANSI_escape_code ; les consoles Linux/Mac
+	// les supportent normalement par défaut.
+	bibliotheque_cours::activerCouleursAnsi();
+	#pragma endregion
+	
+	// Trait de separation
+	static const string trait =
+		"═════════════════════════════════════════════════════════════════════════";
+
+	// Ouverture des fichiers binaires
+	ifstream fichierHeros("heros.bin", ios::binary);
+	ifstream fichierVilains("vilains.bin", ios::binary);
+	fichierHeros.exceptions(ios::failbit);
+	fichierVilains.exceptions(ios::failbit);
+
+	//TODO: Votre code pour le main commence ici
+	
+}
