9125518a03c28abb0c3e774b8fa31d8d8f93f419
Youssef Haddak <youssef.haddak@etud.polymtl.ca>
2026-03-18 11:06:27 -0400

    Ajout du main TD4

--- TD4-H26-Fichiers/main.cpp
+++ TD4-H26-Fichiers/main.cpp
@@ -1,5 +1,9 @@
 ﻿#include <fstream>
+#include <vector>
 #include "bibliotheque_cours.hpp"
+#include "Heros.hpp"
+#include "Vilain.hpp"
+#include "VilainHeros.hpp"
 using namespace std;
 
 using UInt8  = uint8_t;
@@ -64,5 +68,77 @@ int main()
 	fichierVilains.exceptions(ios::failbit);
 
 	//TODO: Votre code pour le main commence ici
-	
+	vector<Heros>       heros;
+	vector<Vilain>      vilains;
+	vector<Personnage*> personnages;
+
+	UInt16 nHeros = lireUint16(fichierHeros);
+	heros.reserve(nHeros);
+	for (UInt16 i = 0; i < nHeros; i++) {
+		string nom = lireString(fichierHeros);
+		string parution = lireString(fichierHeros);
+		string ennemi = lireString(fichierHeros);
+
+		UInt8 nAllies = lireUint8(fichierHeros);
+		vector<string> allies;
+		allies.reserve(nAllies);
+		for (UInt8 j = 0; j < nAllies; j++)
+			allies.push_back(lireString(fichierHeros));
+
+		heros.push_back(Heros(nom, parution, ennemi, allies));
+	}
+
+	UInt16 nVilains = lireUint16(fichierVilains);
+	vilains.reserve(nVilains);
+	for (UInt16 i = 0; i < nVilains; i++) {
+		string nom = lireString(fichierVilains);
+		string parution = lireString(fichierVilains);
+		string objectif = lireString(fichierVilains);
+		vilains.push_back(Vilain(nom, parution, objectif));
+	}
+
+	auto afficherVecteur = [&](auto& vecteur, const string& palette) {
+		for (auto& p : vecteur) {
+			as_ref(p).changerCouleur(palette);
+			as_ref(p).afficher(cout);
+			cout << trait << "\n";
+		}
+		};
+
+	afficherVecteur(heros, "heros");
+	afficherVecteur(vilains, "vilain");
+
+	// La ligne suivante ne compile pas (copie protected) - laissée en commentaire intentionnellement.
+	// Personnage p = heros[0];
+
+	for (auto& h : heros)
+		personnages.push_back(&h);
+	for (auto& v : vilains)
+		personnages.push_back(&v);
+
+	afficherVecteur(personnages, "");
+
+	int indexVilain = 0;
+	int indexHeros = 0;
+	for (int i = 0; i < (int)heros.size(); i++) {
+		bool vilainEstValide = true;
+		for (int k = 0; k < (int)vilains.size(); k++) {
+			if (vilains[k].getNom() == heros[i].getEnnemi() && k == indexVilain) {
+				vilainEstValide = false;
+				break;
+			}
+		}
+		if (vilainEstValide) {
+			indexHeros = i;
+			break;
+		}
+	}
+
+	VilainHeros fusion(vilains[indexVilain], heros[indexHeros]);
+	fusion.changerCouleur("vilainHeros");
+	fusion.afficher(cout);
+	cout << trait << "\n";
+
+	personnages.push_back(&fusion);
+	afficherVecteur(personnages, "");
 }
